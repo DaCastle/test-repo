8a47c79d92d92b9ea409566ea42e4cd946b00229
DaCastle <dakota.castleberg18@gmail.com>
2019-11-11 20:06:16 -0600

    please work

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from 'react'
 
-export default function MyComponent() {
-  const { cat } = props;
-  console.log("cat");
+export default function MyComponent () {
+  const { cat } = props
+
+  console.log('cat')
   return (
     <div>
-      <h1>I was given a
-        {cat}
-        !</h1>
+      <h1>I was given a{cat}!</h1>
     </div>
   )
 }
-
