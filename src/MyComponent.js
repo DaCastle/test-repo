c1fdd9a42d7857e70bae0704f58740bd95d94035
DaCastle <dakota.castleberg18@gmail.com>
2019-11-14 13:35:26 -0600

    adding lazy loaded image

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import FeatureA from './FeatureA'
 import FeatureB from './FeatureB'
+import styles from './MyComponent.module.css'
+import LazyLoad from 'react-lazyload'
 
 export default function MyComponent (props) {
   const { pet } = props
@@ -15,6 +17,12 @@ export default function MyComponent (props) {
       <FeatureB />
       <h1>I was given a {pet}!</h1>
       <button onClick={methodDoesNotExist}>Break the world</button>
+      <div className={styles.container}>
+        <LazyLoad height={200}>
+          {' '}
+          <img alt='kitty' src={'http://placekitten.com/500/200'} />
+        </LazyLoad>
+      </div>
     </div>
   )
 }
